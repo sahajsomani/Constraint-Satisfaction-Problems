2ba5d73b92b1b4fcbc773eb3985232c47b9ffdde
kimn1944 <45519304+kimn1944@users.noreply.github.com>
2019-03-01 13:30:39 -0500

    Update CSP.java

--- CSP.java
+++ CSP.java
@@ -4,6 +4,31 @@ public abstract class CSP { // abstract structure of our CSP
 	private String name;
 	private ArrayList<Variable> variables;
 	private ArrayList<Constraint> constraints;
+import java.util.*;
+
+public abstract class CSP { // abstract structure of our CSP
+	private String name;
+	private ArrayList<Variable> variables;
+	private ArrayList<Constraint> constraints;
+
+	public CSP(String name, ArrayList<Variable> variables, ArrayList<Constraint> constraints) {
+		this.name = name;
+		this.variables = variables;
+		this.constraints = constraints;
+	} // end constructor
+
+	public String getName() { return this.name; }
+
+	public ArrayList<Variable> getVars() { return this.variables; }
+
+	public ArrayList<Constraint> getConst() { return this.constraints; }
+
+	public void setName(String name) { this.name = name; }
+
+	public void setVars(ArrayList<Variable> variables) { this.variables = variables; }
+
+	public void setConst(ArrayList<Constraint> constraints) { this.constraints = constraints; }
+} // end class
 
 	public CSP(String name, ArrayList<Variable> variables, ArrayList<Constraint> constraints) {
 		this.name = name;
