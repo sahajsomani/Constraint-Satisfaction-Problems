3bae4fae0b834048acf76789085eede9a506d1d5
kimn1944 <45519304+kimn1944@users.noreply.github.com>
2019-03-01 12:06:21 -0500

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,66 +1,62 @@
-public class Solver {
-  private CSP csp;
-  private HashMap<String, String> solution;
+import java.util.*;
 
-  public Solver(CSP csp) {
-    this.csp = csp;
-    this.solution = new HashMap<String, String>();
-    this.stack = new Stack<Variable>();
-  } //end constructor
+public class Solver { // Solver class
+	private CSP csp;
+	private int amountAssigned;
 
-  public CSP getCSP() { return this.csp; }
+	public Solver(CSP csp) {
+		this.csp = csp;
+		this.amountAssigned = 0;
+	} // end constructor
 
-  public void setCSP(CSP csp) { this.csp = csp; }
+	public CSP getCSP() { return this.csp; }
 
-  public HashMap<String, String> getSolution() {
-    if(this.completeAssignment()) {
-      return this.solution;
-    } else {
-      System.out.println(this.csp.getName + " has no solution.");
-      return new HashMap<String, String>();
-    }
-  }
+	public void setCSP(CSP csp) { this.csp = csp; }
 
-  public void backTrackingSearch() {
-    if(this.completeAssignment(csp)) {
-      return;
-    }
+	public void backTrackingSearch() {
+		if (this.completeAssignment()) {
+			return;
+		}
+		Variable variable = this.selectUnassignedVariable();
+		System.out.println(variable.getName());
+		Domain domain = variable.getDomain();
+		ArrayList<Value<?>> values = domain.getValuesList();
+		this.amountAssigned++;
+		for (Value<?> value : values) {
+			variable.setValue(value);
+			if (this.isConsistent()) {
+				this.backTrackingSearch();
+			} else {
+				variable.setValue(null);
+			}
+		}
+		variable.setAssigned(false);
+		this.amountAssigned--;
+	} // end backtrackingSearch
 
-    Variable variable = this.selectUnassignedVariable();
-    Domain domain = variable.getDomain();
-    ArrayList<Object implements Comparable> values = domain.getList();
-    for(Object value : values) {
-      variable.setValue(value);
-        if(this.isConsistent()) {
-          this.backTrackingSearch();
-        } else {
-          variable.setValue(null);
-        }
-    }
-    return;
-  } //end backtrackingSearch
+	private boolean completeAssignment() {
+		return (this.csp.getVars().size() == this.amountAssigned);
+	} // end completeAssignment
 
-  private boolean completeAssignment() {
-    return (this.csp.getVars().size() == this.solution.size());
-  } //end completeAssignment
+	private Variable selectUnassignedVariable() {
+		ArrayList<Variable> variables = this.csp.getVars();
+		for (Variable variable : variables) {
+			if (!variable.getAssigned()) {
+				variable.setAssigned(true);
+				return variable;
+			}
+		}
+		return null;
+	} // end selectUnassignedVariable
 
-  private Variable selectUnassignedVariable() {
-    Collection<Variable> variables = this.csp.getVars().values();
-    for(Variable variable : variables) {
-      if(!variable.getAssigned) {
-        return variable;
-      }
-    }
-  } //end selectUnassignedVariable
+	private boolean isConsistent() {
+		ArrayList<Constraint> constraints = this.csp.getConst();
+		for (Constraint constraint : constraints) {
+			if (!constraint.isSatisfied()) {
+				return false;
+			}
+		}
+		return true;
+	} // end isConsistent
 
-  private boolean isConsistent() {
-    ArrayList<Constraint> constraints = this.csp.getConst();
-    for(Constraint constraint : constraints) {
-      if(!constraint.isSatisfied()) {
-        return false;
-      }
-    }
-    return true;
-  } //end isConsistent
-
-} //end class
+} // end class
