431c3c2a64e3f1a0dcb5d03b6486d1dacc585a4e
kimn1944 <45519304+kimn1944@users.noreply.github.com>
2019-03-01 12:04:34 -0500

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,29 +1,29 @@
-public abstract class Variable {
-  private String name;
-  private Domain domain;
-  private boolean assigned;
-  private Object value;
+public class Variable { // abstract structure of Variable class
+	private String name;
+	private Domain domain;
+	private boolean assigned;
+	private Value<?> value;
 
-  public Variable(String name, Domain domain) {
-    this.name = name;
-    this.domain = domain;
-    this.assigned = false;
-    this.value = null;
-  } //end constructor
+	public Variable(String name, Domain domain) {
+		this.name = name;
+		this.domain = domain;
+		this.assigned = false;
+		this.value = null;
+	} // end constructor
 
-  public String getName() { return this.name; }
+	public String getName() { return this.name; }
 
-  public Domain getDomain() { return this.domain; }
+	public Domain getDomain() { return this.domain; }
 
-  public boolean getAssigned() { return this.assigned; }
+	public boolean getAssigned() { return this.assigned; }
 
-  public Object getValue() { return this.value; }
+	public Value<?> getValue() { return this.value; }
 
-  public void setName(String name) { this.name = name; }
+	public void setName(String name) { this.name = name; }
 
-  public void setDomain(Domain domain) { this.domain = domain; }
+	public void setDomain(Domain domain) { this.domain = domain; }
 
-  public void setAssigned(boolean assigned) { this.assigned = assigned; }
+	public void setAssigned(boolean assigned) { this.assigned = assigned; }
 
-  public void setValue(Object value) { this.value = value; }
-} //end class
+	public void setValue(Value<?> value) { this.value = value; }
+} // end class
